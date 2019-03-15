809f5695fe48aae264ab258a580aa948ce43b6fd
KaySyed <waqlearns@gmail.com>
2019-03-15 13:07:10 +0000

    calculating angles and errors on lanes

--- testCV.cpp
+++ testCV.cpp
@@ -67,7 +67,7 @@ int main(int argc, char **argv)
     }
 
     vector<Point2f> centerpoints;
-
+    int countx = 0;
     for (int i = 0; i < whitePointsL.size(); i++)
     {
         if (whitePointsL[i].y == whitePointsR[i].y && whitePointsL[i].y > image.rows / 2)
@@ -76,9 +76,27 @@ int main(int argc, char **argv)
             Point p = Point(centerx, whitePointsL[i].y);
             circle(image, p, 1, Scalar(0, 0, 0), 5, 8);
             centerpoints.push_back(p);
+            countx += centerx;
         }
     }
+    float angle; // = atan2(centerpoints[0].y - p2.y, p1.x - p2.x)
+    int centerpointscount = centerpoints.size();
 
+    int trackerror = (image.cols/2)- (countx/centerpointscount);
+
+    cout<<"\nImage center = "<<image.cols/2<<"\nAverage = "<<countx/centerpointscount<<"\nTrack Error = "<<trackerror;
+    if (centerpointscount > 1){
+        Point p1 = Point(centerpoints[0].x, centerpoints[0].y);
+        Point p2 = Point(centerpoints[centerpointscount-1].x, centerpoints[centerpointscount-1].y);
+        angle = atan2(p2.y - p1.y, p2.x - p1.x);
+        line(image, p2, p1, Scalar(0,255,255), 5, 8);
+        angle = angle * 180 / 3.14;
+    }
+    int angleerror = (angle - 90);
+    cout<< "\nAngle = "<<angle;
+    cout<< "\nAngle Error = "<<angleerror<<"\n";
+    String x = "Image center = " + to_string(image.cols/2) + ", Average = " + to_string(countx/centerpointscount) + ", Track Error = " + to_string(trackerror) + ", Angle = " + to_string(angle) + ", Angle Error = " + to_string(angleerror);
+    putText(image, x , Point(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, Scalar(0,0,0), 1.1);
     imshow("test", image);
 
     waitKey(30000);
